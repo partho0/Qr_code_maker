6e7c87dcc0841723b96dc0231af35a86a7ee2d27
Udoy <68466005+partho0@users.noreply.github.com>
2023-11-07 09:31:56 +0600

    gui

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # qr_code
  
+Open the python file on terminal and run it. Give it the data and it will create and show you where the saved file is located
+
+Thank You
--- qr_code1.py
+++ qr_code1.py
@@ -1,14 +1,45 @@
+import tkinter as tk
+import tkinter.filedialog as fd
 import qrcode
-from PIL import Image
 
-qr = qrcode.QRCode(
-    version=1,
-    error_correction=qrcode.constants.ERROR_CORRECT_H,
-    box_size=10,
-    border=4,  # Set the border to 4 modules
-)
+def generate_qr_code():
+    text = input_text.get()
+    if not text:
+        return
 
-qr.add_data("All hail python")
-qr.make(fit=True)
-img = qr.make_image(fill_color="blue", back_color="black")
-img.save("wsqr.png")
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrcode.png")
+
+    result_label.config(text=f"QR Code created at: {img.filename}")
+
+def save_qr_code():
+    file_path = fd.asksaveasfilename(defaultextension=".png", filetypes=[("PNG Files", "*.png")])
+    if file_path:
+        img = qrcode.make(input_text.get())
+        img.save(file_path)
+        result_label.config(text=f"QR Code saved at: {file_path}")
+
+root = tk.Tk()
+root.title("QR Code Generator")
+
+input_text = tk.Entry(root)
+generate_button = tk.Button(root, text="Generate QR Code", command=generate_qr_code)
+save_button = tk.Button(root, text="Save QR Code", command=save_qr_code)
+result_label = tk.Label(root, text="")
+
+input_text.pack()
+generate_button.pack()
+save_button.pack()
+result_label.pack()
+
+root.mainloop()
+
